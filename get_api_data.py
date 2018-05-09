7fcf452b7f823e5ac5607daae04c21ac9aa60db7
Femi Ajayi <femiajayi705@gmail.com>
2018-05-10 00:39:34 +0800

    Refactored the code and added an environment file

--- get_api_data.py
+++ get_api_data.py
@@ -1,36 +1,43 @@
-# Trying it with TMDB simple
-
-
 import tmdbsimple as tmdb
-import urllib2
-import webbrowser
 import movie
+import env
+import os
 
-tmdb.API_KEY = '1cd7238da5972f9cf532eeeb3705ccfb' # tmdb key
-YVBURL = 'https://www.youtube.com/watch?v=' # Youtube video base url
- # Search for the movie matrix by the tmdb ID (Hard coded for testing)
-
+# This is set in the module called env.py this file is ignored by default to protect secret keys
+tmdb.API_KEY = os.environ['TMDB_API_KEY']
+ 
 # Function to get the move object
 def get_tmdb_object(movie_id):
 	search = tmdb.Movies(movie_id)
 	return search
 
-
-def make_trailer_link(movie_search, YVBURL):
+# Function to make the youtube trailer link
+def make_trailer_link(movie_search):
+	# Youtube video base url
+	base_url = 'https://www.youtube.com/watch?v='
+	# Get video information
 	movie_search.videos()
+	# Get youtube video sub string from the result
 	youtube_video_key = movie_search.results[0]['key']
-	youtube_video_url = YVBURL + youtube_video_key
+	# Construct full youtube link
+	youtube_video_url = base_url + youtube_video_key
 	return youtube_video_url
 
+# fuction to make the paoster link
 def make_poster_links(poster_path):
+	# tmdb base url
 	base_url = 'http://image.tmdb.org/t/p/'
+	# Poster size
 	size = 'w154'
+	# contruct the full link
 	poster_url = base_url + size + poster_path
 	return poster_url
 
 # Set movie object attributes
 def create_movie_player_object(movie_search):
-	movie_search.info() # Get the movie properties
-	movie_player_object = movie.Movie(movie_search.title, movie_search.overview, make_poster_links(movie_search.poster_path), make_trailer_link(movie_search, YVBURL) )
+	# Get the movie properties
+	movie_search.info()
+	# Set the movie atrributes from the movie class
+	movie_player_object = movie.Movie(movie_search.title, movie_search.overview, make_poster_links(movie_search.poster_path), make_trailer_link(movie_search) )
 	return movie_player_object
 
--- movie_player.py
+++ movie_player.py
@@ -1,14 +1,25 @@
 import fresh_tomatoes
 import get_api_data
 
-matrix_search = get_api_data.get_tmdb_object(603)
+""" The movie player creates the movie objects and then passes the list of movies to the fresh tomatoes module """
 
-matrix_movie = get_api_data.create_movie_player_object(matrix_search)
 
-se7en_search = get_api_data.get_tmdb_object(807)
+# Function to create a list of movie objects from a list of tmdb ids
 
-se7en_movie = get_api_data.create_movie_player_object(se7en_search)
+def create_movie_list(tmdb_movie_ids):
+	tmdb_movie_list = []
+	for tmdb_movie_id in tmdb_movie_ids:
+		tmdb_movie_search = get_api_data.get_tmdb_object(tmdb_movie_id)
+		tmdb_movie = get_api_data.create_movie_player_object(tmdb_movie_search)
+		tmdb_movie_list.append(tmdb_movie)
+	return tmdb_movie_list
 
-movies = [matrix_movie, se7en_movie]
 
-fresh_tomatoes.open_movies_page(movies)
+# List of movie ids from the tmdb database
+tmdb_movie_ids = [603, 807, 629]
+
+# Create a list of tmdb movie objets
+tmdb_movies = create_movie_list(tmdb_movie_ids)
+
+# Pass the list of movie objects to the open movies fuction from the fresh tomatoes module
+fresh_tomatoes.open_movies_page(tmdb_movies)
