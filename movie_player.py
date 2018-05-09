1f89826bc3ddc982ceeec5b4f973b07a0d243266
Femi Ajayi <femiajayi705@gmail.com>
2018-05-09 22:23:22 +0800

    Added another movie

--- movie_player.py
+++ movie_player.py
@@ -5,6 +5,10 @@ matrix_search = get_api_data.get_tmdb_object(603)
 
 matrix_movie = get_api_data.create_movie_player_object(matrix_search)
 
-movies = [matrix_movie]
+se7en_search = get_api_data.get_tmdb_object(807)
+
+se7en_movie = get_api_data.create_movie_player_object(se7en_search)
+
+movies = [matrix_movie, se7en_movie]
 
 fresh_tomatoes.open_movies_page(movies)
